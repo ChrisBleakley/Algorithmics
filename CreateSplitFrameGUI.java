d2e4d4b724425097d161ba3e922a2ca09eef7f11
“gavinkeaveneygit config --global user.email “gavin.keaveney@ucdconnect.ie <chubbenblobb33@gmail.com>
2016-02-12 14:04:10 +0000

    Force push

--- CreateSplitFrameGUI.java
+++ CreateSplitFrameGUI.java
@@ -1,15 +1,28 @@
-
+/*
+ * Algorithmics
+ * 14708689 Orla Cullen
+ * 05641349 Gavin Keaveney
+ * 14343826 Jonathan Sweeney
+ * 
+ */
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
+
+import javax.swing.JTextField;
+
 
 public class CreateSplitFrameGUI extends JFrame implements UserInterface {
 
@@ -21,6 +34,9 @@ public class CreateSplitFrameGUI extends JFrame implements UserInterface {
 
 	private static final int FRAME_WIDTH = 1000; // must be even
 	private static final int FRAME_HEIGHT = 600;
+
+	private String play1 = "Player1";
+	private String play2 = "Player2";
 
 	public CreateSplitFrameGUI() {// constructor
 		setTitle("WELCOME TO RISK");
@@ -34,6 +50,7 @@ public class CreateSplitFrameGUI extends JFrame implements UserInterface {
 		createTextDisplay();// Calling the function to create Text Display Panel
 		createGraphPanel();// Calling the function that creates the Graph Panel
 		createUserInputDialog();// Calling the function that creates UserInput Panel
+
 		/*
 		 * The following code creates the split in the frame creates the
 		 * horizontal and the vertical split
@@ -52,7 +69,7 @@ public class CreateSplitFrameGUI extends JFrame implements UserInterface {
 		 * sets the vertical split while passing the textpanel into the left
 		 * most panel of frame
 		 */
-		
+
 		horizontalSplit.setLeftComponent(textPanel);
 		/*
 		 * sets the vertical split while passing the graphpanel into the right
@@ -83,23 +100,52 @@ public class CreateSplitFrameGUI extends JFrame implements UserInterface {
 		graphPanel = new JPanel();
 		graphPanel.setLayout(new FlowLayout());
 		graphPanel.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));// size of graph area
-																				
+
 		graphPanel.add(new JLabel("Insert Graph Here:"), BorderLayout.NORTH);
 		graphPanel.setBackground(Color.white);
 
 	}
 
+	public void assignName(String name, int i) {
+
+		if (i==0){
+			play1=name;
+		}
+		else if (i==1){
+			play2=name;
+		}
+	}
+
+
 	public void createUserInputDialog() {
+
 		userInputPanel = new JPanel();
 		userInputPanel.setLayout(new BorderLayout());
 		userInputPanel.setPreferredSize(new Dimension(100, 50));
 		userInputPanel.setMinimumSize(new Dimension(100, 50));
 
-		userInputPanel.add(new JLabel("User Input Dialog:"), BorderLayout.NORTH);
+		JLabel label = new JLabel("Welcome to Risk");
+		userInputPanel.add(label, BorderLayout.NORTH);
 		userInputPanel.setBackground(Color.yellow);
-		userInputPanel.add(new JTextArea(), BorderLayout.CENTER);
 
+		JTextField textArea = new JTextField();
+		userInputPanel.add(textArea, BorderLayout.CENTER);
+		JButton button = new JButton("Enter Player Names");
+		userInputPanel.add(button, BorderLayout.WEST);
+
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				play1 = JOptionPane.showInputDialog(null, "Enter Player 1 Name", "Risk", JOptionPane.INFORMATION_MESSAGE);
+				play2 = JOptionPane.showInputDialog(null, "Enter Player 2 Name", "Risk", JOptionPane.INFORMATION_MESSAGE);
+				button.setText("Submit");
+				label.setText("Welcome to Risk " + play1 + " & " + play2);
+			}
+		});      
 	}
+
+
+
 
 	public static void main(String[] args) {
 		CreateSplitFrameGUI interfaceFrame = new CreateSplitFrameGUI();
