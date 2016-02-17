b58731324e2a451f8244ae673ba8a65168e7c13d
Gavin Keaveney <Gavin@Gavins-MacBook.local>
2016-02-17 23:42:09 +0000

    Changes to image input

--- Map.java
+++ Map.java
@@ -3,19 +3,25 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+
 import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import javax.swing.JPanel;
 
 public class Map extends JPanel{
 	Map(){
-
+		
 	}
 
 	public static final int[] CONTINENT_IDS = {0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,5,5};
-
+	
 	public static final String[] COUNTRY_NAMES = {
 			"Ontario","Quebec","NW Territory","Alberta","Greenland","E United States","W United States","Central America","Alaska",
 			"Great Britain","W Europe","S Europe","Ukraine","N Europe","Iceland","Scandinavia",
@@ -23,8 +29,8 @@ public class Map extends JPanel{
 			"E Australia","New Guinea","W Australia","Indonesia",
 			"Venezuela","Peru","Brazil","Argentina",
 			"Congo","N Africa","S Africa","Egypt","E Africa","Madagascar"};  // for reference
-
-	public static final int[][] ADJACENT = { 
+	
+		public static final int[][] ADJACENT = { 
 			{4,1,5,6,3,2},    // 0
 			{4,5,0},
 			{4,0,3,8},
@@ -67,122 +73,132 @@ public class Map extends JPanel{
 			{11,18,40,37},
 			{39,18,41,38,36,37},  //40
 			{38,40}
-	};
+		};
+		
+		private static final int[][] COUNTRY_COORD = {
+				{191,150},     // 0
+				{255,161},
+				{146,86},
+				{123,144},
+				{314,61},
+				{205,235},
+				{135,219},
+				{140,299},
+				{45,89},
+				{370,199},
+				{398,280},      // 10
+				{465,270},
+				{547,180},
+				{460,200},
+				{393,127},
+				{463,122},
+				{628,227},
+				{679,332},
+				{572,338},
+				{861,213},
+				{645,152},      // 20
+				{763,70},
+				{827,94},
+				{751,360},
+				{750,140},
+				{695,108},
+				{760,216},
+				{735,277},
+				{889,537},
+				{850,429},
+				{813,526},       // 30
+				{771,454},
+				{213,352},
+				{221,426},
+				{289,415},
+				{233,523},
+				{496,462},
+				{440,393},
+				{510,532},
+				{499,354},
+				{547,432},        // 40
+				{586,545}
+			};
+		private BufferedImage image;
+		public void paintComponent(Graphics grphcs) {
+			
+			super.paintComponent(grphcs);
+				Graphics2D g2d = (Graphics2D) grphcs;
+				 try {
+			            image = ImageIO.read(new File("src/risk.jpg"));
+			            
 
-	private static final int[][] COUNTRY_COORD = {
-			{191,150},     // 0
-			{255,161},
-			{146,86},
-			{123,144},
-			{314,61},
-			{205,235},
-			{135,219},
-			{140,299},
-			{45,89},
-			{370,199},
-			{398,280},      // 10
-			{465,270},
-			{547,180},
-			{460,200},
-			{393,127},
-			{463,122},
-			{628,227},
-			{679,332},
-			{572,338},
-			{861,213},
-			{645,152},      // 20
-			{763,70},
-			{827,94},
-			{751,360},
-			{750,140},
-			{695,108},
-			{760,216},
-			{735,277},
-			{889,537},
-			{850,429},
-			{813,526},       // 30
-			{771,454},
-			{213,352},
-			{221,426},
-			{289,415},
-			{233,523},
-			{496,462},
-			{440,393},
-			{510,532},
-			{499,354},
-			{547,432},        // 40
-			{586,545}
-	};
-
-	public void paintComponent(Graphics grphcs) {
-		super.paintComponent(grphcs);
-		Graphics2D g2d = (Graphics2D) grphcs;
-		paintNodes(g2d);
-		paintConnectingLines(g2d);
-		showCountryNames(g2d);
-	}
-
-	public void paintNodes(Graphics2D g2d){
-		for(int i=0;i<42;i++){	
-			switch (CONTINENT_IDS[i]) {
-			case 0:  g2d.setColor(Color.yellow);
-			break;
-			case 1:  g2d.setColor(Color.blue);
-			break;
-			case 2:  g2d.setColor(Color.green);
-			break;
-			case 3:  g2d.setColor(Color.orange);
-			break;
-			case 4:  g2d.setColor(Color.red);
-			break;
-			case 5:  g2d.setColor(Color.MAGENTA);
-			break;
+			        } catch (IOException ioe) {
+			            System.out.println("Could not read in the pic");
+			            //System.exit(0);
+			        }
+				g2d.drawImage(image,0,0,this);
+				paintNodes(g2d);
+				paintConnectingLines(g2d);
+				showCountryNames(g2d);
+		}
+		
+		public void paintNodes(Graphics2D g2d){
+			for(int i=0;i<42;i++){	
+				switch (CONTINENT_IDS[i]) {
+				case 0:  g2d.setColor(Color.yellow);
+                		 break;
+	            case 1:  g2d.setColor(Color.blue);
+	                     break;
+	            case 2:  g2d.setColor(Color.green);
+	                     break;
+	            case 3:  g2d.setColor(Color.magenta);
+	                     break;
+	            case 4:  g2d.setColor(Color.red);
+	                     break;
+	            case 5:  g2d.setColor(Color.cyan);
+	                     break;
 			}
-			g2d.fillOval(COUNTRY_COORD[i][0] - 10,COUNTRY_COORD[i][1] - 10,20,20);
-		}	
-	}
-
-
-	public void paintConnectingLines(Graphics2D g2d){
-		g2d.setColor(Color.black);
-		for(int i=0;i<42;i++){	
-			for(int j=0;j<6;j++){	
-				if((i!=8 || j!= 2)&& (i!=22 || j!= 0)){
-					try{
-						g2d.drawLine(COUNTRY_COORD[i][0], COUNTRY_COORD[i][1], COUNTRY_COORD[ADJACENT[i][j]][0], COUNTRY_COORD[ADJACENT[i][j]][1]);
+					g2d.fillOval(COUNTRY_COORD[i][0] - 10,COUNTRY_COORD[i][1] - 10,20,20);
+				}	
+		}
+		
+		
+		public void paintConnectingLines(Graphics2D g2d){
+			g2d.setColor(Color.black);
+			for(int i=0;i<42;i++){	
+				for(int j=0;j<6;j++){	
+					if((i!=8 || j!= 2)&& (i!=22 || j!= 0)){
+						try{
+							g2d.drawLine(COUNTRY_COORD[i][0], COUNTRY_COORD[i][1], COUNTRY_COORD[ADJACENT[i][j]][0], COUNTRY_COORD[ADJACENT[i][j]][1]);
+						}
+						catch(ArrayIndexOutOfBoundsException e){
+							break;
+						}
 					}
-					catch(ArrayIndexOutOfBoundsException e){
-						break;
+					else{
+						g2d.drawLine(COUNTRY_COORD[8][0], COUNTRY_COORD[8][1], -50,  COUNTRY_COORD[22][1]);
+						g2d.drawLine(COUNTRY_COORD[22][0], COUNTRY_COORD[22][1], 1050,  COUNTRY_COORD[8][1]);
 					}
-				}
-				else{
-					g2d.drawLine(COUNTRY_COORD[8][0], COUNTRY_COORD[8][1], -50,  COUNTRY_COORD[22][1]);
-					g2d.drawLine(COUNTRY_COORD[22][0], COUNTRY_COORD[22][1], 1050,  COUNTRY_COORD[8][1]);
 				}
 			}
 		}
-	}
+		
+		public void showCountryNames(Graphics2D g2d){
+			Font font = new Font("Serif", Font.BOLD, 16);
+		    g2d.setFont(font);
+			for(int i=0;i<42;i++){
+			
+				 g2d.drawString(COUNTRY_NAMES[i],COUNTRY_COORD[i][0] - (2*COUNTRY_NAMES[i].length() +10) ,COUNTRY_COORD[i][1] - 15 );	
+			
 
-	public void showCountryNames(Graphics2D g2d){
-		Font font = new Font("Serif", Font.BOLD, 16);
-		g2d.setFont(font);
-		for(int i=0;i<42;i++){
-
-			g2d.drawString(COUNTRY_NAMES[i],COUNTRY_COORD[i][0] - (2*COUNTRY_NAMES[i].length() +10) ,COUNTRY_COORD[i][1] - 15 );	
-
-
+			}
 		}
-	}
-
-	public void showArmyNumber(Graphics2D g2d){
-		for(int i=0;i<42;i++){
-
+		
+		public void showArmyNumber(Graphics2D g2d){
+			for(int i=0;i<42;i++){
+				
+			}
 			
 		}
-
-	}
+		
 
 }
 
-
-
+	
+		
