8e2b0b4095434e558f26eab332094908c2b19843
Gavin Keaveney <Gavin@Gavins-MacBook.local>
2016-03-02 18:04:10 +0000

    Output countries issued to players

--- PlayGame.java
+++ PlayGame.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /* Algorithmics
  * 14708689 Orla Cullen
  * 05641349 Gavin Keaveney
@@ -6,24 +10,36 @@
 public class PlayGame {
 	SplitFrameGUI interfaceFrame = new SplitFrameGUI();
 	PlayGame(){
-	
-		
+
+
 		interfaceFrame.pack();
 		interfaceFrame.setVisible(true);
-		int playerId;
-		String name;
+//		int playerId;
+		String name1;
+		String name2;
+		List<Integer> arrayList = new ArrayList<Integer>();
 
 		// get player names
-		for (playerId = 0; playerId < GameData.NUM_PLAYERS; playerId++) {
-			interfaceFrame.displayString("Enter the name of player " + (playerId + 1));
-			name = interfaceFrame.getCommand();
-			interfaceFrame.displayString("Welcome --> " + name);
-		}
-	
+		interfaceFrame.displayString("Enter the name of player 1");
+		name1 = interfaceFrame.getCommand();
+		interfaceFrame.displayString("Welcome --> " + name1);
+		interfaceFrame.displayString("Enter the name of player 2");
+		name2 = interfaceFrame.getCommand();
+		interfaceFrame.displayString("Welcome --> " + name2);
+
+		// allocates territories
+		arrayList = deal();
+		printNames(0,9,arrayList,name1);
+		printNames(9,18,arrayList,name2);
+		printNames(18,24,arrayList,"Neutral 1");
+		printNames(24,30,arrayList,"Neutral 2");
+		printNames(30,36,arrayList,"Nuetral 3");
+		printNames(36,42,arrayList,"Neutral 4");
+		
 		interfaceFrame.displayString("Enter roll ");
 		rolling();
-	
-		
+
+
 	}
 	public  void rolling(){
 		do {
@@ -42,9 +58,9 @@ public class PlayGame {
 				break;
 			}
 		} while (true);
-		
+
 	}
-	
+
 	public int rollDice() {
 		Die die = new Die();
 		die.roll();
@@ -69,8 +85,28 @@ public class PlayGame {
 		return winner;
 
 	}
-	
-	
-	}
 
+	// creates number list 0-41 and randomises  
+	public List<Integer> deal() {
+		int i=0;
+		List<Integer> arrayList = new ArrayList<Integer>();
+
+		for (i=0;i<	42 ;i++){
+			arrayList.add(i);
+		}
+
+		Collections.shuffle(arrayList);
+		
+		return arrayList;
+	}
 	
+	public void printNames(int j, int k, List<Integer> arrayList, String name){
+		String nameList = "";
+		for (int i=j;i<k;i++){
+			nameList += (GameData.COUNTRY_NAMES[arrayList.get(i)] + ", ");
+		}
+		interfaceFrame.displayString(name +" has received " + nameList + "\n");
+
+	}
+}
+
