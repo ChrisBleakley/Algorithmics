4a37a00fe1c907f1789da07f1ac741e54be321ef
OrlaC15 <orla.cullen@ucdconnect.ie>
2016-04-22 13:27:28 +0100

    get reinforcements  still needs work

--- AlgorithmicsBot/Algorithmics.java
+++ AlgorithmicsBot/Algorithmics.java
@@ -24,18 +24,65 @@ public class Algorithmics implements Bot {
 		return(command);
 	}
 // Orla
-	public String getReinforcement () {
+	public String getReinforcement() {
 		String command = "";
-		// put your code here
-		command = GameData.COUNTRY_NAMES[(int)(Math.random() * GameData.NUM_COUNTRIES)];
-		command = command.replaceAll("\\s", "");
-		command += " 1";
-		return(command);
+		int myId = 0;
+
+		for (int i = 0; i < 42; i++) {
+			myId = player.getId();
+			if ((board.getOccupier(i) == 0 || board.getOccupier(i) == 1) && (i > 27)) {
+
+				if (GameData.CONTINENT_IDS[i] > 2 && board.getNumUnits(i) < 7) {
+					command = GameData.COUNTRY_NAMES[(int) (Math.random() * 41 - 27) + 27];
+					command = command.replaceAll("\\s", "");
+					command += " 3";
+
+				}
+
+				else if (GameData.CONTINENT_IDS[i] < 2 && board.getNumUnits(i) >= 1 && board.getNumUnits(i) < 7) {
+					command = command.replaceAll("\\s", "");
+					command += " 1";
+
+				} else {
+					command = "";
+					command = GameData.COUNTRY_NAMES[(int) (Math.random() * GameData.NUM_COUNTRIES)];
+					command = command.replaceAll("\\s", "");
+					command += " 1";
+				}
+			}
+			if ((board.getOccupier(i) == 0 || board.getOccupier(i) == 1) && (i < 27)) {
+
+				if (GameData.CONTINENT_IDS[i] < 7 && board.getNumUnits(i) > 3) {
+					command = GameData.COUNTRY_NAMES[i];
+					command = command.replaceAll("\\s", "");
+					command += " 1";
+
+				} else if (GameData.CONTINENT_IDS[i] < 7 && board.getNumUnits(i) <= 3) {
+					command = GameData.COUNTRY_NAMES[i];
+					command = command.replaceAll("\\s", "");
+					command += " 1";
+				} else {
+					command = "";
+					command = GameData.COUNTRY_NAMES[(int) (Math.random() * GameData.NUM_COUNTRIES)];
+					command = command.replaceAll("\\s", "");
+					command += " 1";
+				}
+			} else if ((board.getOccupier(i) != myId) && (i < 42)) {
+				command = "";
+				command = GameData.COUNTRY_NAMES[(int) (Math.random() * GameData.NUM_COUNTRIES)];
+				command = command.replaceAll("\\s", "");
+				command += " 1";
+
+			}
+
+		}
+
+		return (command);
 	}
 	// Orla
 	public String getPlacement (int forPlayer) {
 		String command = "";
-		// put your code here
+		
 		command = GameData.COUNTRY_NAMES[(int)(Math.random() * GameData.NUM_COUNTRIES)];
 		command = command.replaceAll("\\s", "");
 		return(command);
