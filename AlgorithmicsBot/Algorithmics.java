c0a2cc33ad10215d8c6fb63f9c009d01e9fd1a98
OrlaC15 <orla.cullen@ucdconnect.ie>
2016-04-26 21:02:27 +0100

    CardExchange method

--- AlgorithmicsBot/Algorithmics.java
+++ AlgorithmicsBot/Algorithmics.java
@@ -113,9 +113,109 @@ public class Algorithmics implements Bot {
 	}
 	
 	public String getCardExchange () {
+		boolean flagI= checkI();
+		boolean flagA = checkA();
+		boolean flagC= checkC();
+		boolean flagF=checkFullset();
+		boolean flagIW= checkIW();
+		boolean flagIWW= checkIWW();
+		boolean flagAW=checkAW();
+		boolean flagAWW= checkAWW();
+		boolean flagCW=checkCW();
+		boolean flagCWW= checkCWW();
+		boolean flagCAW=checkCAW();
+		boolean flagIAW= checkIAW();
+		boolean flagICW= checkICW();
 		String command = "";
-		// put your code here
-		command = "skip";
+		if (player.isOptionalExchange()){
+				
+			
+			if (flagI){
+				command="III";
+			}
+			if (flagA){
+				command="AAA";
+			}
+			if (flagC){
+				command="CCC";
+			}
+			if (flagF){
+				command="IAC";
+			}
+			if (flagIW){
+				command="IIW";
+			}
+			if (flagIWW){
+				command="IWW";
+			}
+			if (flagAW){
+				command="AAW";
+			}
+			if (flagAWW){
+				command="AWW";
+			}
+			if (flagCW){
+				command="CCW";
+			}
+			if (flagCWW){
+				command="CWW";
+			}
+			if (flagCAW){
+				command="CAW";
+			}
+			if (flagIAW){
+				command="IAW";
+			}
+			if (flagICW){
+				command="ICW";
+			}
+			
+		}
+	
+		if (player.isForcedExchange ()){
+				
+			
+			if (flagI){
+				command="III";
+			}
+			if (flagA){
+				command="AAA";
+			}
+			if (flagC){
+				command="CCC";
+			}
+			if (flagF){
+				command="IAC";
+			}
+			if (flagIW){
+				command="IIW";
+			}
+			if (flagIWW){
+				command="IWW";
+			}
+			if (flagAW){
+				command="AAW";
+			}
+			if (flagAWW){
+				command="AWW";
+			}
+			if (flagCW){
+				command="CCW";
+			}
+			if (flagCWW){
+				command="CWW";
+			}
+			if (flagCAW){
+				command="CAW";
+			}
+			if (flagIAW){
+				command="IAW";
+			}
+			if (flagICW){
+				command="ICW";
+			}
+			
+		}
 		return(command);
 	}
 // Jon
@@ -173,5 +273,136 @@ public class Algorithmics implements Bot {
 		command = GameData.COUNTRY_NAMES[fortifyToCountry] + GameData.COUNTRY_NAMES[fortifyFromCountry] + numberToMove;
 		return(command);
 	}
+	private boolean checkI(){
+		int[] infantry;
+		infantry = new int[] {0,0,0};
+		
+		boolean flagI= player.isCardsAvailable(infantry);
+		if (flagI ==false){
+		return false;}
+		else
+			return flagI;
+	}
+		private boolean checkC(){
+			int[] cavalry;
+			cavalry = new int[] {1,1,1};
+			
+			boolean flagC= player.isCardsAvailable(cavalry);
+			if (flagC ==false){
+			return false;}
+			else
+				return flagC;
+		}
+		
+		private boolean checkA(){
+		int[] art;
+		art = new int[] {2,2,2};
+		
+		boolean flagA= player.isCardsAvailable(art);
+		if (flagA ==false){
+		return false;}
+		else
+			return flagA;
+	}
+		private boolean checkFullset(){
+			int[] full;
+			full = new int[] {0,1,2};
+			
+			boolean flagF= player.isCardsAvailable(full);
+			if (flagF ==false){
+			return false;}
+			else
+				return flagF;
+		}
+		private boolean checkIW(){
+			int[] infw1;
+			infw1= new int[] {0,0,3};
+			
+			boolean flagIW= player.isCardsAvailable(infw1);
+			if (flagIW ==false){
+			return false;}
+			else
+				return flagIW;
+		}
+		private boolean checkIWW(){
+			int[] infw1;
+			infw1= new int[] {0,3,3};
+			
+			boolean flagIWW= player.isCardsAvailable(infw1);
+			if (flagIWW ==false){
+			return false;}
+			else
+				return flagIWW;
+		}
+		private boolean checkAW(){
+			int[] infw1;
+			infw1= new int[] {2,2,3};
+			
+			boolean flagAW= player.isCardsAvailable(infw1);
+			if (flagAW ==false){
+			return false;}
+			else
+				return flagAW;
+		}
+		private boolean checkAWW(){
+			int[] infw1;
+			infw1= new int[] {2,3,3};
+			
+			boolean flagAWW= player.isCardsAvailable(infw1);
+			if (flagAWW ==false){
+			return false;}
+			else
+				return flagAWW;
+		}
+		private boolean checkCW(){
+			int[] infw1;
+			infw1= new int[] {1,1,3};
+			
+			boolean flagCW= player.isCardsAvailable(infw1);
+			if (flagCW ==false){
+			return false;}
+			else
+				return flagCW;
+		}
+		private boolean checkCWW(){
+			int[] infw1;
+			infw1= new int[] {1,3,3};
+			
+			boolean flagCWW= player.isCardsAvailable(infw1);
+			if (flagCWW ==false){
+			return false;}
+			else
+				return flagCWW;
+		}
+		private boolean checkCAW(){
+			int[] infw1;
+			infw1= new int[] {1,2,3};
+			
+			boolean flagCAW= player.isCardsAvailable(infw1);
+			if (flagCAW ==false){
+			return false;}
+			else
+				return flagCAW;
+		}
+		private boolean checkIAW(){
+			int[] infw1;
+			infw1= new int[] {0,2,3};
+			
+			boolean flagIAW= player.isCardsAvailable(infw1);
+			if (flagIAW ==false){
+			return false;}
+			else
+				return flagIAW;
+		}
+		private boolean checkICW(){
+			int[] infw1;
+			infw1= new int[] {0,1,3};
+			
+			boolean flagICW= player.isCardsAvailable(infw1);
+			if (flagICW ==false){
+			return false;}
+			else
+				return flagICW;
+		}
 
 }
